04efdc3815aac9582557ecb5a2a6750b5593b87b
raarce <raarce@gmail.com>
2024-08-14 14:07:41 +0000

    lab01 added

---
+++ lab01.cpp
@@ -0,0 +1,11 @@
+// Rafael Arce Nazario (raarce@gmail.com)
+// A program to practice modifications and compilation
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "This is a 1!" << std::endl;
+  
+  return 0;
+}
